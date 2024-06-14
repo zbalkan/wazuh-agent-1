22ac5634cbe0ee57afed4d7902c95ac21782010f
jr0me <joaquin.romera@wazuh.com>
2024-06-14 10:29:58 -0300

    Add flag to Logger

--- poc/curl/logger.hpp
+++ poc/curl/logger.hpp
@@ -11,6 +11,11 @@ class Logger
 public:
     static void log(const std::string& tag, const std::string& message)
     {
+        if (!LOGGING_ENABLED)
+        {
+            return;
+        }
+
         // Get the current time
         auto now = std::chrono::system_clock::now();
         auto in_time_t = std::chrono::system_clock::to_time_t(now);
@@ -22,4 +27,8 @@ public:
         // Output the log message
         std::cout << "[" << ss.str() << "] [" << tag << "] " << message << std::endl;
     }
+
+    static bool LOGGING_ENABLED;
 };
+
+bool Logger::LOGGING_ENABLED = true;
