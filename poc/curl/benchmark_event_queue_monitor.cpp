d023d8d2f7097fbe5b2053bdbaf20d0b925de884
jr0me <joaquin.romera@wazuh.com>
2024-06-14 11:04:16 -0300

    Use fixture for benchmarks

--- poc/curl/benchmark_event_queue_monitor.cpp
+++ poc/curl/benchmark_event_queue_monitor.cpp
@@ -13,28 +13,43 @@
 #include "db/sqlite_wrapper.hpp"
 #include "logger.hpp"
 
-
-static void BM_EventQueueMonitor_DispatchPendingEvents(benchmark::State& state)
+class EventQueueMonitorFixture : public benchmark::Fixture
 {
-    Logger::LOGGING_ENABLED = false;
-
-    auto onEvent = [](const std::string& event_data) -> bool
+public:
+    void SetUp(const ::benchmark::State& state) override
     {
-        return true;
-    };
+        Logger::LOGGING_ENABLED = false;
 
+        onEvent = [](const std::string& event_data) -> bool
+        {
+            return true;
+        };
+
+        monitor = std::make_unique<EventQueueMonitor<SQLiteWrapper>>(onEvent);
+    }
+
+    void TearDown(const ::benchmark::State& state) override
+    {
+        monitor.reset();
+    }
+
+    std::unique_ptr<EventQueueMonitor<SQLiteWrapper>> monitor;
+    std::function<bool(const std::string&)> onEvent;
+};
+
+BENCHMARK_DEFINE_F(EventQueueMonitorFixture, DispatchPendingEvents)(benchmark::State& state)
+{
     for (auto _ : state)
     {
-        EventQueueMonitor<SQLiteWrapper> monitor(onEvent);
-
         std::vector<Event> pending_events(state.range(0));
+
         for (int i = 0; i < state.range(0); ++i)
         {
-            monitor.eventQueue->InsertEvent(i, "event_data", "event_type");
+            monitor->eventQueue->InsertEvent(i, "event_data", "event_type");
         }
     }
 }
 
-BENCHMARK(BM_EventQueueMonitor_DispatchPendingEvents)->Arg(10)->Arg(100)->Arg(1000);
+BENCHMARK_REGISTER_F(EventQueueMonitorFixture, DispatchPendingEvents)->Arg(10)->Arg(100)->Arg(1000);
 
 BENCHMARK_MAIN();
