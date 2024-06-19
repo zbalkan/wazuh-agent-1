2e9f8a76a12abbbe1030ad9845a9b75b43c18ad8
jr0me <joaquin.romera@wazuh.com>
2024-06-19 13:49:27 -0300

    Make StdThreadManager thread safe

--- poc/curl/std_thread_manager.hpp
+++ poc/curl/std_thread_manager.hpp
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <thread>
+#include <mutex>
 #include <vector>
 
 #include "thread_manager_wrapper.hpp"
@@ -9,16 +10,19 @@
 class StdThreadManager : public ThreadManagerWrapper
 {
 private:
+    std::mutex threads_mutex;
     std::vector<std::thread> threads;
 
 public:
 
     void CreateThreadImpl(std::function<void()> func) override {
+        std::lock_guard<std::mutex> lock(threads_mutex);
         threads.emplace_back(std::move(func));
     }
 
     void CleanUpJoinableThreads() override
     {
+        std::lock_guard<std::mutex> lock(threads_mutex);
         auto it = std::remove_if(threads.begin(),
                                  threads.end(),
                                  [](std::thread& t)
@@ -31,11 +35,15 @@ public:
                                      return false;
                                  });
 
-        threads.erase(it, threads.end());
+        if (it != threads.end())
+        {
+            threads.erase(it, threads.end());
+        }
     }
 
     void JoinAll() override
     {
+        std::lock_guard<std::mutex> lock(threads_mutex);
         for (auto& thread : threads)
         {
             if (thread.joinable())
