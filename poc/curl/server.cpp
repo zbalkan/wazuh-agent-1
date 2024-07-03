3e0441cad20a07f246d89cff30981705da408c62
jr0me <joaquin.romera@wazuh.com>
2024-07-03 00:30:32 -0300

    Make mock server multithreaded to handle multiple calls

--- poc/curl/server.cpp
+++ poc/curl/server.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 #include <string>
 #include <unordered_map>
+#include <thread>
+#include <vector>
 
 #include "server/listener.hpp"
 
@@ -16,11 +18,24 @@ int main(int argc, char* argv[])
     auto const address = net::ip::make_address("0.0.0.0");
     auto const port = static_cast<unsigned short>(8080);
 
-    net::io_context ioc {1};
+    net::io_context ioc;
 
     std::make_shared<listener>(ioc, tcp::endpoint {address, port})->run();
 
-    ioc.run();
+    // Run the I/O service on multiple threads
+    std::vector<std::thread> threads;
+    auto num_threads = std::thread::hardware_concurrency();
+    for (auto i = 0; i < num_threads; ++i)
+    {
+        threads.emplace_back([&ioc] {
+            ioc.run();
+        });
+    }
+
+    for (auto& t : threads)
+    {
+        t.join();
+    }
 
     return 0;
 }
