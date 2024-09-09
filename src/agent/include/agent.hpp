wazuh/wazuh-agent

feat: message processing routines rework.
Also added coroutine to peridically push messages into queue.

src/agent/include/agent.hpp
@@ -28,4 +28,28 @@ private:
     communicator::Communicator m_communicator;
     command_manager::CommandManager m_commandManager;
     MultiTypeQueue m_agentQueue;
+
+    boost::asio::awaitable<void> PushTestMessages()
+    {
+        using namespace std::chrono_literals;
+        const auto executor = co_await boost::asio::this_coro::executor;
+        std::unique_ptr<boost::asio::steady_timer> expTimer = std::make_unique<boost::asio::steady_timer>(executor);
+
+        // const nlohmann::json dataContent = {{"command", {{"name", "001"}, {"type", "stateless"}}},
+        //                                    {"origin", {{"serverName", "node01"}, {"moduleName", "upgradeModule"}}},
+        //                                    {"parameters", {{"error", 0}, {"data", "Command received"}}},
+        //                                    {"status", "Pending"}};
+
+        int count = 0;
+        while (true)
+        {
+            std::string strCmdName = "upgradeModule" + std::to_string(count++);
+            const nlohmann::json dataContent = {{"command", strCmdName}, {"status", "Pending"}};
+            const Message messageToSend {MessageType::COMMAND, dataContent, "CommandManager"};
+            m_agentQueue.push(messageToSend);
+
+            expTimer->expires_after(3000ms);
+            co_await expTimer->async_wait(boost::asio::use_awaitable);
+        }
+    }
 };

zbalkan/wazuh-agent-1

src/agent/include/agent.hpp
@@ -29,4 +29,28 @@ private:
     communicator::Communicator m_communicator;
     command_handler::CommandHandler m_commandHandler;
     MultiTypeQueue m_agentQueue;
+
+    boost::asio::awaitable<void> PushTestMessages()
+    {
+        using namespace std::chrono_literals;
+        const auto executor = co_await boost::asio::this_coro::executor;
+        std::unique_ptr<boost::asio::steady_timer> expTimer = std::make_unique<boost::asio::steady_timer>(executor);
+
+        // const nlohmann::json dataContent = {{"command", {{"name", "001"}, {"type", "stateless"}}},
+        //                                    {"origin", {{"serverName", "node01"}, {"moduleName", "upgradeModule"}}},
+        //                                    {"parameters", {{"error", 0}, {"data", "Command received"}}},
+        //                                    {"status", "Pending"}};
+
+        int count = 0;
+        while (true)
+        {
+            std::string strCmdName = "upgradeModule" + std::to_string(count++);
+            const nlohmann::json dataContent = {{"command", strCmdName}, {"status", "Pending"}};
+            const Message messageToSend {MessageType::COMMAND, dataContent, "CommandHandler"};
+            m_agentQueue.push(messageToSend);
+
+            expTimer->expires_after(3000ms);
+            co_await expTimer->async_wait(boost::asio::use_awaitable);
+        }
+    }
 };