wazuh/wazuh-agent

feat: Added DispatchCommand function

src/agent/src/agent.cpp
@@ -23,6 +23,17 @@ Agent::~Agent()
 
 void Agent::Run()
 {
+    // add some test command to the queue
+    // const nlohmann::json dataContent = {{"command", {{"name", "001"}, {"type", "stateless"}}},
+    //                                    {"origin", {{"serverName", "node01"}, {"moduleName", "upgradeModule"}}},
+    //                                    {"parameters", {{"error", 0}, {"data", "Command received"}}},
+    //                                    {"status", "Pending"}};
+    //
+
+    const nlohmann::json dataContent = {{"command", "upgradeModule"}, {"status", "Pending"}};
+    const Message messageToSend {MessageType::COMMAND, dataContent, "CommandManager"};
+    m_agentQueue.push(messageToSend);
+
     m_taskManager.EnqueueTask(m_communicator.WaitForTokenExpirationAndAuthenticate());
 
     m_taskManager.EnqueueTask(m_communicator.GetCommandsFromManager(
@@ -43,7 +54,44 @@ void Agent::Run()
             {
                 return std::nullopt;
             }
-            return m_agentQueue.getNext(MessageType::COMMAND);
+            Message m = m_agentQueue.getNext(MessageType::COMMAND);
+
+            std::cout << "COMMAND retrieved from Queue:" << std::endl;
+
+            // pop message from queue
+            m_agentQueue.pop(MessageType::COMMAND);
+
+            nlohmann::json jdata = m.data.at(0);
+            for (auto& [key, value] : jdata.items())
+            {
+                std::cout << key << ": " << value << std::endl;
+            }
+
+            std::cout << "Data inside data:" << std::endl;
+
+            nlohmann::json jdataData = m.data.at(0).at("data");
+            for (auto& [key, value] : jdataData.items())
+            {
+                std::cout << key << ": " << value << std::endl;
+            }
+
+            // change status and push again
+            // if (jdataData.at("status") == "Pending")
+            //{
+            //     std::cout << "Message status is Pending. Updating status and pushing back." << std::endl;
+            //     jdataData["status"] = "InProcess";
+            //     Message newMessage(MessageType::COMMAND, jdataData, "CommandManager");
+            //     // m_agentQueue.push(newMessage);
+            //     // return m;
+            // }
+
+            return m;
+        },
+        [this](Message& cmd) -> int
+        {
+            std::cout << "Dispatching command" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            return 1;
         }));
 
     m_signalHandler.WaitForSignal();

zbalkan/wazuh-agent-1

src/agent/src/agent.cpp
@@ -28,6 +28,17 @@ Agent::~Agent()
 
 void Agent::Run()
 {
+    // add some test command to the queue
+    // const nlohmann::json dataContent = {{"command", {{"name", "001"}, {"type", "stateless"}}},
+    //                                    {"origin", {{"serverName", "node01"}, {"moduleName", "upgradeModule"}}},
+    //                                    {"parameters", {{"error", 0}, {"data", "Command received"}}},
+    //                                    {"status", "Pending"}};
+    //
+
+    const nlohmann::json dataContent = {{"command", "upgradeModule"}, {"status", "Pending"}};
+    const Message messageToSend {MessageType::COMMAND, dataContent, "CommandHandler"};
+    m_agentQueue.push(messageToSend);
+
     m_taskManager.EnqueueTask(m_communicator.WaitForTokenExpirationAndAuthenticate());
 
     m_taskManager.EnqueueTask(m_communicator.GetCommandsFromManager(
@@ -50,7 +61,44 @@ void Agent::Run()
             {
                 return std::nullopt;
             }
-            return m_agentQueue.getNext(MessageType::COMMAND);
+            Message m = m_agentQueue.getNext(MessageType::COMMAND);
+
+            std::cout << "COMMAND retrieved from Queue:" << std::endl;
+
+            // pop message from queue
+            m_agentQueue.pop(MessageType::COMMAND);
+
+            nlohmann::json jdata = m.data.at(0);
+            for (auto& [key, value] : jdata.items())
+            {
+                std::cout << key << ": " << value << std::endl;
+            }
+
+            std::cout << "Data inside data:" << std::endl;
+
+            nlohmann::json jdataData = m.data.at(0).at("data");
+            for (auto& [key, value] : jdataData.items())
+            {
+                std::cout << key << ": " << value << std::endl;
+            }
+
+            // change status and push again
+            // if (jdataData.at("status") == "Pending")
+            //{
+            //     std::cout << "Message status is Pending. Updating status and pushing back." << std::endl;
+            //     jdataData["status"] = "InProcess";
+            //     Message newMessage(MessageType::COMMAND, jdataData, "CommandHandler");
+            //     // m_agentQueue.push(newMessage);
+            //     // return m;
+            // }
+
+            return m;
+        },
+        [this](Message& cmd) -> int
+        {
+            std::cout << "Dispatching command" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            return 1;
         }));
 
     m_signalHandler.WaitForSignal();